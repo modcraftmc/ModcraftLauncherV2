e06b26a54f5cd47dfbad85782740f72d09bf162d
modcraftmc <contact@modcraftmc.fr>
2020-03-30 22:01:45 +0200

    Basic setup

---
+++ src/main/java/fr/modcraftmc/launcher/ModcraftLauncher.java
@@ -0,0 +1,15 @@
+package fr.modcraftmc.launcher;
+
+import fr.modcraftmc.launcher.ui.ModcraftApplication;
+import javafx.application.Application;
+
+public class ModcraftLauncher {
+
+    public static void main(String[] args) {
+
+        //TODO: check system
+
+        Application.launch(ModcraftApplication.class, args);
+
+    }
+}
---
+++ src/main/java/fr/modcraftmc/launcher/ui/ModcraftApplication.java
@@ -0,0 +1,12 @@
+package fr.modcraftmc.launcher.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ModcraftApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
